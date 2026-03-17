12a4397f8a5b787a233627f6f1a3a353fe7525d0
Bárbara <bafenixx@gmail.com>
2026-03-17 14:58:06 -0300

    Update login.php

--- view/login.php
+++ view/login.php
@@ -34,7 +34,7 @@
                     $cadastro = new Cadastro($usuarioLogin, $senhaLogin, $_SESSION['usuarioCadastro'], $_SESSION['senhaCadastro']);
 
                     if($_SESSION['senhaCadastro'] == $senhaLogin && $_SESSION['usuarioCadastro'] == $usuarioLogin){
-                        header('location: tarefas.php');
+                        header('location: caminho.php');
                     }else{
                         echo "Usuário ou senha incorreto!";
                     }
